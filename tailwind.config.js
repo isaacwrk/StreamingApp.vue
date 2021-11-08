836ce63f4c834cfa2d98b7537dd84d997f3eaebb
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-08 19:11:47 -0300

    adding base hex colors to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors:{
+                "strong-purple": "#8b3cfa",
+                "base-purple":"#f2e8ff",
+                "buttom-pink":"#f974ca",
+                "base-white":"#ffffff",
+                "yellow-star":"#f0bb40"
+            }
+        },
     },
     variants: {
         extend: {},
