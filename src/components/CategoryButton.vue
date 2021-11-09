caaf855fc1da4d73602a57142d7868f0e68639fb
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-09 11:05:40 -0300

    creating categoryButtons component

---
+++ src/components/CategoryButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex flex-nowrap ml-10 mr-10 justify-center">
+        <div class="bg-bottom-purple w-28 h-24 p-5 rounded text-center hover:text-white hover:bg-strong-purple text-strong-purple text-xl">
+            <i class="uil uil-search"></i>
+            <p>Search</p>
+        </div>
+        <div class="bg-bottom-purple ml-2 w-28 h-24 p-5 rounded text-center hover:text-white hover:bg-strong-purple text-strong-purple text-xl">
+            <i class="uil uil-desktop"></i>
+            <p class="pb-1">TV Shows</p>
+        </div>
+        <div class="bg-bottom-purple ml-2 w-28 h-24 p-5 rounded text-center hover:text-white hover:bg-strong-purple text-strong-purple text-xl">
+            <i class="uil uil-film"></i>
+            <p>Movies</p>
+        </div>
+        <div class="bg-bottom-purple ml-2 w-28 h-24 p-5 rounded text-center hover:text-white hover:bg-strong-purple text-strong-purple text-xl">
+            <i class="uil uil-basketball"></i>
+            <p>Sports</p>
+        </div>
+    </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const categoryButton = defineComponent({
+});
+
+export default categoryButton;
+</script>
+
+<style>
+
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,18 +1,25 @@
 <template>
-    <div class="bg-base-purple min-h-full h-72 rounded-bl-3xl">
+    <div class="bg-base-purple min-h-full w-min-full h-96 rounded-bl-3xl">
         <div class="flex flex-wrap p-10">
             <img src="~@/assets/profilepic.jpg" class="h-20 rounded border-solid border-2 border-white" alt="Cyclops profile picture">
             <h2 class="ml-4 mt-2 font-medium text-font-black text-xl">Mr. Smith</h2>
-            <a href="#" class="absolute right-10 mt-4 bg-bottom-purple text-text-purple p-1 text-lg rounded">Upgrade</a>
+            <a href="#" class="absolute right-10 mt-5 w-28 text-center bg-bottom-purple text-text-purple p-1 text-lg rounded">Upgrade</a>
             <p class="absolute left-20 ml-14 mt-10 text-sub-gray">Basic Subscription</p>
         </div>
+        <CategoryButtons/>
+        <p class="text-font-black font-bold ml-10 mt-6">Continue Watching</p>
     </div>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
+//components
+import CategoryButtons from '@/components/CategoryButton.vue';
 
 const name = defineComponent({
+    components:{
+        CategoryButtons
+    }
 });
 
 export default name;
