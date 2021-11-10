714f59582a48e3dd92dea3920642d9a02515e4d6
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-10 16:35:45 -0300

    adding star rate and align

--- src/views/playlist/PlaylistView.vue
+++ src/views/playlist/PlaylistView.vue
@@ -1,23 +1,41 @@
 <template>
-    <div >
-        <PlaylistPoster/>
-        <div class="relative z-10 pt-15 pb-32">
-            <div class="absolute bg-buttom-pink hover:bg-pink-500 -bottom-56 right-8 rounded-full w-14 h-14 ">
-                <i class="uil uil-play flex justify-center text-xl p-5 font-black text-base-white"></i>
+    <div>
+        <header class="relative bg-cover h-80 w-full bg-dark-poster rounded-bl-4xl rounded rounded-br-xl">
+            <div class="absolute bg-buttom-pink hover:bg-pink-500 -bottom-8 right-8 rounded-full w-14 h-14 ">
+                <i class="uil uil-play flex justify-center text-2xl p-4 font-black text-base-white"></i>
             </div>
+        </header>
+        <div class="flex flex-wrap">
+            <p class="uppercase font-medium text-font-black text-sm ml-10 mt-6">Dark(2017)</p>
+        </div>
+        <div class="flex flex-1 text-sub-gray text-xs ml-10 mt-1">
+            <p class="mr-2">Adventure</p>
+            <p class="mr-2">Mistery</p>
+            <p class="mr-2">Thriller</p>
+            <p>Sci Fi</p>
+        </div>
+        <div class="flex flex-1 ml-10 mt-1 text-base">
+            <i class="fas fa-star mr-2 text-yellow-star"></i>
+            <i class="fas fa-star  mr-2 text-yellow-star"></i>
+            <i class="fas fa-star mr-2 text-yellow-star"></i>
+            <i class="fas fa-star  mr-2 text-yellow-star"></i>
+            <i class="fas fa-star text-yellow-star opacity-50"></i>
         </div>
     </div>
+  
+    
+   
     
         
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
-import PlaylistPoster from '@/components/PlaylistPoster.vue';
+
 
 const PlaylistView = defineComponent({
     components:{
-        PlaylistPoster
+        
     }
 });
 
@@ -25,5 +43,7 @@ export default PlaylistView;
 </script>
 
 <style>
-
+.bg-dark-poster{
+    background-image: url('~@/assets/darkposter.jpg');
+}
 </style>
