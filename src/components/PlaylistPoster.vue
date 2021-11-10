a1d0c9ab2d5c9eea46146d54997e954eca7ab3bf
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-10 14:58:58 -0300

    creating playlistposter component

---
+++ src/components/PlaylistPoster.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="absolute top-0 bg-cover right-0 h-80 w-full bg-dark-poster rounded-bl-4xl rounded rounded-br-xl"></div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const PlaylistPoster = defineComponent({
+});
+
+export default PlaylistPoster;
+</script>
+
+<style>
+.bg-dark-poster{
+    background-image: url('~@/assets/darkposter.jpg');
+}
+</style>
--- src/views/playlist/PlaylistView.vue
+++ src/views/playlist/PlaylistView.vue
@@ -1,11 +1,24 @@
 <template>
-    <div>playlist</div>
+    <div >
+        <PlaylistPoster/>
+        <div class="relative z-10 pt-15 pb-32">
+            <div class="absolute bg-buttom-pink hover:bg-pink-500 -bottom-56 right-8 rounded-full w-14 h-14 ">
+                <i class="uil uil-play flex justify-center text-xl p-5 font-black text-base-white"></i>
+            </div>
+        </div>
+    </div>
+    
+        
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
+import PlaylistPoster from '@/components/PlaylistPoster.vue';
 
 const PlaylistView = defineComponent({
+    components:{
+        PlaylistPoster
+    }
 });
 
 export default PlaylistView;
