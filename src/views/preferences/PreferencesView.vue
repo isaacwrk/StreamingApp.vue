ab24853a74fd3a256ac5e2df91268ab3f65bdb1c
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-11 09:22:04 -0300

    adding setting page

--- src/views/preferences/PreferencesView.vue
+++ src/views/preferences/PreferencesView.vue
@@ -1,11 +1,17 @@
 <template>
-    <div>settings</div>
+    <div>
+        <BuildingPage icon="uil uil-setting" textInfo="Settings Page in progress" />
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BuildingPage from '@/components/BuildingPage.vue';
 
 const PreferencesView = defineComponent({
+    components:{
+        BuildingPage
+    }
 });
 
 export default PreferencesView;
